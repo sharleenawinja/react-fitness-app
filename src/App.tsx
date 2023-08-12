28312516984b169595dacbab7b897994615b4828
sharleenawinja <awinjasharleen@gmail.com>
2023-08-12 21:44:44 +0300

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import Navbar from "@/scenes/navbar";
 import { useEffect, useState } from "react";
+
+import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
 import { SelectedPage } from "@/shared/types";
 
 function App() {
@@ -27,6 +29,7 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   );
 }
